b811e9a99a834093f13788143026c15729d5c1e7
calypsoriebe <ckriebe@gmail.com>
2022-10-20 19:51:41 -0400

    tsting if my branch works

--- favorite-recipes/src/App.js
+++ favorite-recipes/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          aloha from calypso branch
         </a>
       </header>
     </div>
