a3623bb158c6a577115a7a2be73fb2baee2fb037
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-10-20 21:16:28 -0400

    form entry

--- favorite-recipes/src/App.js
+++ favorite-recipes/src/App.js
@@ -7,19 +7,12 @@ function App() {
   return (
         <Form>
           <Form.Group className="mb-3">
-            <Form.Label>recipe name</Form.Label>
-            <Form.Control type="recipe" placeholder="Enter recipe" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Label> Email address</Form.Label>
+            <Form.Control size="lg" type="text" placeholder="Favorite Recipe" />
           </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
