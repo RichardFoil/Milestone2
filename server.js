f022185f5fcd6784228d43ac60f87fe4efecd3d5
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-10-23 11:28:58 -0400

    first new-rich commit

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const PORT = 4000
+const mongoose = require('mongoose')
+
+
+require('dotenv').config()
+
+// Mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+
+// LISTEN
+app.listen(PORT, () => {
+  
+  console.log('listening on port', PORT);
+  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+  
 })
