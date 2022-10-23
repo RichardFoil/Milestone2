ac149f1ec46727198a59d8379bde11b860d30511
lawrencegraham <lgraham274@yahoo.com>
2022-10-23 12:06:17 -0400

    routes created in controller

---
+++ controllers/recipes_controller.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const recipes = express.Router()
+//const recipes = require('../models/recipes.js')
+    
+    //index routes
+recipes.get('/', (req, res) => {
+
+    res.json({message: 'Get all the recipes'})
+})
+
+        //show route 
+recipes.get('/:id', (req, res) => {
+
+    res.json({message: 'Get One the recipes'})
+})
+        // post route
+recipes.post('/', (req, res) => {
+   res.json({message: 'Create route hit!! recipes!!'})
+  })
+
+        //delete route
+  recipes.delete('/:id', (req, res) => {
+    res.json({message: 'Delete this route!! recipes!!'})
+      })
+
+      //edit route
+      breads.get('/:id/edit', (req, res) => {
+    
+      })
+      
+  
+
+
+module.exports = recipes
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 const express = require ('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+
+const recipesController = require("./controllers/recipes_controller");
+
+app.use("/recipes",recipesController)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
