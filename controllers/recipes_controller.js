d64b5f534fbf0886293662fd31aedf403b118520
lawrencegraham <lgraham274@yahoo.com>
2022-10-25 20:48:12 -0400

    progress day 3

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -1,38 +1,41 @@
-const express = require('express')
-const recipes = express.Router()
-const Recipes = require('../models/models.js')
-    
-    //index routes
-recipes.get('/',async (req, res) => {
-    console.log('WE SMACKED RECIPES ROUTE!!!')
-    const foundRecipes = await Recipes.find()
-    res.json(foundRecipes)
-})
+const express = require("express");
+const recipes = express.Router();
+const Recipes = require("../models/models.js");
 
-        //show route 
-recipes.get('/:id',async (req, res) => {
-    const foundRecipes = await Recipes.findById(req.params.id)
-    res.json(foundRecipes)
-    })
-        // post route
-recipes.post('/',async (req, res) => {
-    const foundRecipes = await Recipes.findById(req.params.id)
-   res.json(foundRecipes)
-    })
+//index routes
+recipes.get("/", async (req, res) => {
+  console.log("WE SMACKED RECIPES ROUTE!!!");
+  const foundRecipes = await Recipes.find();
+  res.json(foundRecipes);
+});
 
-        //delete route
-  recipes.delete('/:id',async (req, res) => {
-    const foundRecipes = await Recipes.findById(req.params.id)
-    res.json(foundRecipes)
-    })
+//show route
+recipes.get("/:id", async (req, res) => {
+  const foundRecipes = await Recipes.findById(req.params.id);
+  res.json(foundRecipes);
+});
+// post route
+recipes.post("/", async (req, res) => {
+  const foundRecipes = await Recipes.create(req.body);
+  res.json(foundRecipes);
+});
 
-      //edit route
-     recipes.get('/:id/edit',async (req, res) => {
-        const foundRecipes = await Recipes.findById(req.params.id)
-        res.json(foundRecipes)
-     })
-      
-  
+//delete route
+recipes.delete("/:id", async (req, res) => {
+  const foundRecipes = await Recipes.delete(req.params.id);
+  res.json(foundRecipes);
+});
 
+//edit route
+recipes.get("/:id/edit", async (req, res) => {
+  const foundRecipes = await Recipes.findById(req.params.id);
+  res.json(foundRecipes);
+});
 
-module.exports = recipes
+//gallery/Homepage route 
+recipes.get("/", async (req, res) => {
+    const foundRecipes = await Recipes.find();
+    res.json(foundRecipes);
+  });
+
+module.exports = recipes;
--- favorite-recipes/src/App.js
+++ favorite-recipes/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 import RecipeGallery from './recipeGallery';
 import AddRecipe from './recipeAdd';
@@ -7,13 +7,15 @@ import UpdateRecipe from './recipeUpdate';
 
 function App() {
   
-  return <div className='App'>
-    <h2>The point of our database is to be able to share your favorite recipes with others!visit the existing recipe gallery or add your recipe now!</h2>
-    <Router>
+  return (
+  <div className='App'>
+    <h1> HELLLOO!</h1>
+     <h2>The point of our database is to be able to share your favorite recipes with others!visit the existing recipe gallery or add your recipe now!</h2>
+    <BrowserRouter>
       <header>
         <div className='navBar'>
           <h1>OUR FAVORITE RECIPE DATABASE</h1>
-          <ul>
+          {/* <ul>
             <li>
               <Link to='/'>HOME PAGE</Link>
             </li>
@@ -29,20 +31,21 @@ function App() {
             <li>
             <Link to='/delete'>RECIPE DELETE</Link>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </header> 
      <div className='display'>
-        <Routes>
+        {/* <Routes>
           <Route path='/gallery' element={<RecipeGallery />} />
           <Route path='/add' element={<AddRecipe />} />
           <Route path='/update' element={<UpdateRecipe />} />
           <Route path='/delete' element={<DeleteRecipe />} />
-        </Routes>
+        </Routes> */}
       </div>
-    </Router> 
+    </BrowserRouter> 
     
   </div>
+  )
 }
 
 export default App;
