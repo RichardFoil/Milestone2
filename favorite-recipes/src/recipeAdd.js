84b8a299a66678f362b2202ce13c41f9e5045ea4
calypsoriebe <ckriebe@gmail.com>
2022-10-27 20:40:21 -0400

    Added some more css components

--- favorite-recipes/src/recipeAdd.js
+++ favorite-recipes/src/recipeAdd.js
@@ -9,59 +9,59 @@ function AddRecipe() {
         <Form>
             <Row className="mb-3 NamePart">
                 <Form.Group as={Col} controlId="formGirdChefName">
-                <Form.Label>Chef Name:</Form.Label>
-                <Form.Control type="name" placeholder="Chef's name" />
+                    <Form.Label>Chef Name: </Form.Label>
+                    <Form.Control type="name" placeholder="Chef's name" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridRecipeName">
-                <Form.Label>Recipe Name:</Form.Label>
-                <Form.Control type="text" placeholder="Recipe Name" />
+                    <Form.Label>Recipe Name: </Form.Label>
+                    <Form.Control type="text" placeholder="Recipe Name" />
+                </Form.Group>
+            </Row>
+            <Row className="mb-3 directions">
+                <Form.Group className="mb-3 ingredients" controlId="formGridIngredients">
+                    <Form.Label>Ingredients Needed: </Form.Label>
+                    <Form.Control type="text" placeholder="Ingredients" />
+                </Form.Group>
+
+                <Form.Group className="mb-3 instructions" controlId="formGridInstructions">
+                    <Form.Label>Instructions: </Form.Label>
+                    <Form.Control type="text" placeholder="Instructions" />
+                </Form.Group>
+            </Row>
+            <Row className="mb-3 allergies">
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Vegan" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Vegitarian" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Peanuts" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Gluten Free" />
                 </Form.Group>
             </Row>
 
-            <Form.Group className="mb-3 ingredients" controlId="formGridIngredients">
-                <Form.Label>Ingredients</Form.Label>
-                <Form.Control type="text" placeholder="Ingredients" />
-            </Form.Group>
-
-            <Form.Group className="mb-3 instructions" controlId="formGridInstructions">
-                <Form.Label>Instructions</Form.Label>
-                <Form.Control  type="text" placeholder="Instructions" />
-            </Form.Group>
-
-            <Row className="mb-3 allergies">
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Vegan" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Vegitarian" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Peanuts" />
-            </Form.Group>
-            
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Gluten Free" />
-            </Form.Group>
-            </Row>
-
             <Row className="mb-3 TypeOfMeal">
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Breakfast" />
-            </Form.Group>
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Breakfast" />
+                </Form.Group>
 
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Diner" />
-            </Form.Group>
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Diner" />
+                </Form.Group>
 
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Dessert" />
-            </Form.Group>
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Dessert" />
+                </Form.Group>
             </Row>
 
-            <Button variant="primary" type="submit">
+            <Button className="button" variant="primary" type="submit">
                 Submit
             </Button>
         </Form>
